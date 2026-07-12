0326ef1fe12a374b79d600ab04ab29be2f50a00f
Parth Pakhare <parthpakhareducation@gmail.com>
2026-07-12 17:56:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Factorials of large numbers/factorials-of-large-numbers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    vector<int> factorial(int n) {
+        // code here
+        vector<int>ans(1,1);
+        while (n>1){
+            int carry = 0, res=0, size = ans.size();
+            
+            for (int i=0; i<size; i++){
+                res = ans[i]*n + carry;
+                carry = res/10;
+                ans[i] = res%10;
+            }
+            
+            while (carry){
+                ans.push_back(carry%10);
+                carry /= 10;
+            }
+            
+            n--;
+        }
+        
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
